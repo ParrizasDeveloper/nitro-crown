e95afc27d5a2c33a294a878aff25350232a5b86b
parrizasDeveloper <maparrizasd@gmail.com>
2025-08-15 06:06:23 +0200

    responsive main home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,14 +22,16 @@ export default function Home() {
         felx content-center pt-32 z-10 h-dvh w-full overflow-hidden
       `}>
         <div className={`
-          relative flex flex-col justify-center items-center h-[888px]
+          relative flex flex-col justify-center items-center 2xl:h-[888px] xl:h-[700px]
+          lg:h-[600px]
         `}>
           <h1
             ref={titleElement}
             className={`${anton.className}
-            lg:absolute 2xl:text-[18rem] 2xl:top-0 tracking-widest leading-none text-transparent 
+            relative lg:absolute 2xl:text-[18rem] 2xl:top-0 tracking-widest leading-none text-transparent 
             bg-clip-text bg-radial-[at_50%_75%] from-gold/50 via-70% via-neutral-800/50 
-            to-black xl:text-[15rem] xl:top-[100px] lg:text-[12rem] lg:top-[210px]
+            to-black xl:text-[15rem] xl:top-[100px] lg:text-[12rem] lg:top-[100px] md:text-[10rem]
+            sm:text-[8rem] text-[5rem]
           `}
           >
             {lastTitle.current?.toUpperCase()}
@@ -37,15 +39,15 @@ export default function Home() {
           <div
             ref={containerMainCars}
             className={`
-            relative lg:absolute left-0 w-full flex top-[80px] z-20
+            relative lg:absolute w-full flex lg:top-[80px] z-20
           `}
           >
             {mainCars?.map(car => (
               <div key={car.model} className="mainCar w-full shrink-0">
                 <div
                   className={`
-                  relative mx-auto 2xl:w-[1400px] 2xl:top-[10px] xl:w-[900px] 
-                  xl:top-[110px] lg:w-[600px] lg:top-[215px]
+                  relative mx-10 lg:mx-auto 2xl:w-[1400px] 2xl:top-[10px] xl:w-[900px] 
+                  xl:top-[110px] lg:w-[600px] lg:top-[100px]
                   `}
                 >
                   <Image
@@ -60,41 +62,40 @@ export default function Home() {
             ))}
           </div>
           <div className={`
-          absolute 2xl:w-[2000px] aspect-square z-10  rounded-full 2xl:top-[-340px]
-          bg-radial from-neutral-700 to-black from-[-25%] to-80% left-1/2 translate-x-[-50%]
-          -rotate-x-80 border-neutral-500/5 border-8 xl:w-[1400px] xl:top-[-140px]
-          lg:w-[1100px] lg:top-[-15px]
-        `}></div>
+            hidden lg:block absolute 2xl:w-[2000px] aspect-square z-10  rounded-full 2xl:top-[-340px]
+            bg-radial from-neutral-700 to-black from-[-25%] to-80% left-1/2 translate-x-[-50%]
+            -rotate-x-80 border-neutral-500/5 border-8 xl:w-[1400px] xl:top-[-140px]
+            lg:w-[1100px] lg:top-[-130px]
+          `}></div>
 
 
-        <div
-          className={`absolute left-0 top-1/2 -translate-y-1/2 text-gold/25 z-40 h-1/2 w-[300px] 
-          flex justify-center items-center cursor-pointer
-        `}
-          onClick={slideLeft}
-        >
-          <ChevronLeft
-            size={100}
-            className="stroke-1"
-          />
+          <div className={`
+            relative lg:absolute z-30 flex justify-center gap-15 lg:gap-0 lg:justify-between w-full lg:px-10
+          `}>
+              <div
+                className={`
+                  relative text-gold/25 z-40 flex justify-center items-center cursor-pointer
+                `}
+                onClick={slideLeft}
+              >
+                <ChevronLeft
+                  size={100}
+                  className="stroke-1"
+                />
+              </div>
+              <div
+                className={`
+                  relative text-gold/25 z-40 flex justify-center items-center cursor-pointer
+                `}
+                onClick={slideRight}
+              >
+                <ChevronRight
+                  size={100}
+                  className="stroke-1"
+                />
+              </div>
+          </div>
         </div>
-        <div
-          className={`absolute right-0 top-1/2 -translate-y-1/2 text-gold/25 z-40 h-1/2 w-[300px] 
-          flex justify-center items-center cursor-pointer
-        `}
-          onClick={slideRight}
-        >
-          <ChevronRight
-            size={100}
-            className="stroke-1"
-          />
-        </div>
-      </div>
-        
-        
-
-
-        
       </section>
       <section className="h-dvh">
         Hola
