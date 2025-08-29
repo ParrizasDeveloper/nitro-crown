bf3800bb3414e8e83b40572ad0933a7e563316bd
parrizasDeveloper <maparrizasd@gmail.com>
2025-08-29 07:17:56 +0200

    animation navLinks from responsive nav

--- src/components/layout/headerNav/sideNav.tsx
+++ src/components/layout/headerNav/sideNav.tsx
@@ -3,11 +3,25 @@ import Link from "next/link"
 import { anton } from "@/styles/fonts"
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-import { useRef } from "react"
+import { useLayoutEffect, useRef } from "react"
 
 
 export default function SideNav({open, scrollbarWidth}: {open: boolean, scrollbarWidth: number }) {
     const container = useRef<HTMLDivElement | null>(null)
+    const navWords = useRef<Element[]>([])
+
+    useLayoutEffect(() => {
+        const navLinks = document.querySelectorAll(".sideNavLink > span.navlink-name")
+        /*const matrix: Element[][] = []
+
+        navLinks.forEach(link => {
+            const word = link.querySelectorAll("div > span")
+            matrix.push(Array.from(word))
+        })*/
+
+        navWords.current = Array.from(navLinks)
+            
+    }, [])
 
     useGSAP(() => {
         const tl = gsap.timeline()
@@ -20,6 +34,14 @@ export default function SideNav({open, scrollbarWidth}: {open: boolean, scrollba
                     duration: 1,
                     ease: "power4.out"
                 })
+            
+            navWords.current.forEach((word, i) => {
+                tl.from(word.querySelectorAll("div > span"), {
+                    left: "-100%",
+                    stagger: 0.05
+                }, i === 0 ? ">-0.7" : "<+0.1")
+            })
+            
         } else {
             tl
                 .set("body", { overflow: "auto" })
@@ -29,8 +51,8 @@ export default function SideNav({open, scrollbarWidth}: {open: boolean, scrollba
                     duration: 1,
                     ease: "power2.out"
                 })
-
         }
+        
     }, [open])
 
     return (
@@ -39,39 +61,56 @@ export default function SideNav({open, scrollbarWidth}: {open: boolean, scrollba
             ref={container}
             className={`
                 ${anton.className}
-                h-0 text-[clamp(4rem,11vmin,6rem)] overflow-hidden
+                h-0 text-[clamp(1rem,16vmin,7rem)] overflow-hidden
                 lg:hidden fixed w-dvw z-100 bg-neutral
                 justify-center items-center leading-[1.2] text-neutral-light
             `}
         >
             <div className={`
-                absolute w-full h-dvh bg-radial-[at_15%_15%] from-[#3a3a3a]
-                to-60%
+                absolute w-full h-dvh bg-radial-[at_15%_15%] from-[#878787]/40 from-10%
             `}>
                 
             </div>
             <div className={`
-                absolute flex flex-col h-dvh justify-center py-header w-full px-6
+                absolute flex flex-col h-dvh justify-center items-start py-header w-full px-6
                 sm:px-16 md:px-28
             `}>
-                <Link href="" className="flex items-center gap-3">
-                    <span className="text-[0.3em]">01</span>
-                    <span>PROFILE</span>
-                </Link>
-                {
-                    links.map((link, i) => (
-                        <Link
-                            href={link.link}
-                            key={link.name}
-                            className="flex items-center gap-3"
-                        >
-                            <span className="text-[0.3em]">
-                                {`0${i + 2}`}
-                            </span>
-                            <span>{link.name.toUpperCase()}</span>
-                        </Link>
-                    ))
-                }
+                <div id="sideNavLinks">
+                    <Link href="" className="sideNavLink flex items-center gap-3">
+                        <span className="text-[0.3em]">01</span>
+                        <span className="navlink-name flex">
+                            {Array.from("PROFILE").map((char, i) => (
+                                <div key={i} className="relative overflow-hidden">
+                                    <span className="whitespace-pre relative">
+                                        {char}
+                                    </span>
+                                </div>
+                            ))}
+                        </span>
+                    </Link>
+                    {
+                        links.map((link, i) => (
+                            <Link
+                                href={link.link}
+                                key={link.name}
+                                className="sideNavLink flex items-center gap-3"
+                            >
+                                <span className="text-[0.3em]">
+                                    {`0${i + 2}`}
+                                </span>
+                                <span className="navlink-name flex">
+                                    {Array.from(link.name.toUpperCase()).map((char, i) => (
+                                        <div key={i} className="relative overflow-hidden">
+                                            <span className="whitespace-pre relative">
+                                                {char}
+                                            </span>
+                                        </div>
+                                    ))}
+                                </span>
+                            </Link>
+                        ))
+                    }
+                </div>
             </div>
         </nav>
     )
