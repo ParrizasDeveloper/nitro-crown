d4d649d5d73d9f23afb61618bd44989429d92459
parrizasDeveloper <maparrizasd@gmail.com>
2025-09-22 06:13:28 +0200

    mainCars section

--- src/components/ui/home/mainCars.tsx
+++ src/components/ui/home/mainCars.tsx
@@ -1,10 +1,15 @@
 export default function MainCars() {
     return (
-        <section className="relative h-dvh z-50 border text-text overflow-hidden">
+        <section className="relative h-dvh z-50 text-text overflow-hidden">
             <div className={`
-                absolute h-[200px] border origin-top-right -rotate-5 w-[110%] right-0
+                absolute h-[200px] origin-top-right -rotate-5 w-[110%] right-0
+                bg-primary-dark
             `}>Hola</div>
-            <div className={`relative mt-[200px] h-4`}></div>
+            <div className={`
+                relative mt-[200px] h-full bg-primary-dark
+            `}>
+                
+            </div>
         </section>
     )
 }
--- src/components/ui/home/mainSection.tsx
+++ src/components/ui/home/mainSection.tsx
@@ -58,29 +58,33 @@ export default function MainSection() {
                     src="/images/home/bg_main.webp"
                     alt="Background image about a dealership with sport cars"
                     fill
-                    className="z-0 object-cover hidden md:block"
+                    className="z-0 object-cover hidden xl:block"
                     quality={100}
                     priority
+                    
                 />
                 <Image
                     src="/images/home/bg_main_mobile.webp"
                     alt="Background image about a dealership with sport cars"
                     fill
-                    className="z-0 object-cover md:hidden"
+                    className="z-0 object-cover xl:hidden"
                     quality={100}
                     priority
                 />
             </div>
             <div className={`
-                z-30 text-text text-shadow-[0_0_20px_#014996]
+                z-30 text-text
                 text-center text-[clamp(6rem,20vw,30rem)]
                 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 leading-[0.8em]
             `}>
-                <p className="relative top-[0.11em]">BEYOND</p>
+                <p className="relative top-[0.11em] [text-stroke:5px_black]">BEYOND</p>
                 <div className="text-primary flex justify-center">
                     <div
                         ref={dynamicTitle}
-                        className="h-[1em] relative top-[0.11em]"
+                        className={`
+                            ${teko.className} h-[1em] relative top-[0.2em]
+                            text-shadow-[0_0_20px_#0079f9]
+                        `}
                     >
                         {Array.from(swappingTitles[currentTitle]).map((letter, i) => (
                             <span key={i} className="hidden">{letter}</span>
@@ -88,7 +92,7 @@ export default function MainSection() {
                     </div>
                     <div className={`
                         bg-white w-[0.02em] [animation:blink_1.1s_ease-in-out_infinite]
-                        ml-[0.02em] shadow-[0_0_10px_#014996]
+                        ml-[0.02em] shadow-[0_0_10px_#014996] -scale-y-75 relative top-[0.02em]
                     `}></div>
                 </div>
             </div>
