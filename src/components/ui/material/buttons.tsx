eddd83a1498200678ca4bf444425372e9a7f6a23
parrizasDeveloper <maparrizasd@gmail.com>
2025-10-25 06:46:58 +0200

    circle button animation

--- src/components/ui/material/buttons.tsx
+++ src/components/ui/material/buttons.tsx
@@ -1,37 +1,60 @@
 import gsap from "gsap"
+import { useRef } from "react"
 
 export function RoundedPrimaryButton({link, title}: {link?: string, title: string}) {
-    const tl = gsap.timeline()
+    const btnRef = useRef<HTMLDivElement | null>(null)
     
-    function hoverButton(target: HTMLDivElement) {
-        tl.to(target, {
-            ease: "back.in(4)",
-            padding: 0,
-            border: 0,
+    function handleMove(e: React.MouseEvent) {
+        if(!btnRef.current) { return }
+
+        const rect = btnRef.current.getBoundingClientRect()
+        const x = Math.max(0, Math.min(rect.width, e.clientX - rect.left))
+        const y = Math.max(0, Math.min(rect.height, e.clientY - rect.top))
+
+        console.log(x, y)
+
+        gsap.to(btnRef.current.querySelector(".circle-button>div"), {
+            width: "80%",
+            height: "80%",
+            x: (x - rect.width / 2) / 3,
+            y: (y - rect.height / 2) / 4,
+            background: "white",
+            color: "#111317"
         })
     }
 
-    function leaveButton(target: HTMLDivElement) {
-        tl.to(target, {
-            ease: "power1.out",
-            padding: "10px",
-            border: "1px solid #5a5a5a",
+    function handleLeave() {
+        if(!btnRef.current) { return }
+        gsap.to(btnRef.current.querySelector(".circle-button>div"), {
+            width: "90%",
+            height: "90%",
+            x: 0,
+            y: 0,
+            background: "#0079f9",
+            color: "#FAFAFA"
         })
     }
 
     return (
         <div 
-            className={`
-                border border-secondary h-full w-full rounded-full  
-                p-2.5 cursor-pointer
-            `}
-            onMouseEnter={({currentTarget}) => hoverButton(currentTarget)}
-            onMouseLeave={({ currentTarget }) => leaveButton(currentTarget)}
+            ref={btnRef}
+            className="h-full w-full cursor-pointer select-none"
+            onMouseMove={handleMove}
+            onMouseLeave={handleLeave}
         >
-            <div className={`
-                flex justify-center items-center h-full w-full bg-primary rounded-full
-            `}>
-                {title}
+            <div
+                className={`
+                    circle-button 
+                    relative flex justify-center items-center
+                    border border-secondary h-full w-full rounded-full
+                `}
+            >
+                <div className={`
+                    flex justify-center items-center h-[90%] w-[90%]
+                    bg-primary rounded-full 
+                `}>
+                    {title}
+                </div>
             </div>
         </div>
     )
