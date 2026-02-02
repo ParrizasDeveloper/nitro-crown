14625c853128c2d876920145ba5804bc7ce116a0
parrizasDeveloper <maparrizasd@gmail.com>
2026-02-02 18:43:36 +0100

    connection succes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: [
+    '.prisma',
+    '@prisma/client'
+  ]
 };
 
 export default nextConfig;
